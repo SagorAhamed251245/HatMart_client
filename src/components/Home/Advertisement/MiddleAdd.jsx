7f54933ea2c0180411182bca979595303f879d44
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-08-10 18:01:29 +0600

    middle add

---
+++ src/components/Home/Advertisement/MiddleAdd.jsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+const MiddleAdd = () => {
+  return (
+    <section className="">
+      <section className="w-[90%] mx-auto h-[300px] bg-red-300 relative">
+        <Image
+          src="https://i.ibb.co/P1F7nSx/Grocery-Shoping-1.png"
+          layout="fill"
+          objectFit="cover"
+          alt="Middle Add"
+        ></Image>
+      </section>
+      <section>
+        <div>
+
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default MiddleAdd;
