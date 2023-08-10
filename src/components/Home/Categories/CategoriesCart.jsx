6208a5469e6a1a81ff4d09288b28152732acffd1
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-08-10 11:07:47 +0600

    marg

--- src/components/Home/Categories/CategoriesCart.jsx
+++ src/components/Home/Categories/CategoriesCart.jsx
@@ -1,21 +1,17 @@
 import Image from "next/image";
 
-
-
 const CategoriesCart = ({ item }) => {
   const { id, category, icon, sub_category } = item;
- 
+
   return (
-    
-        <div className="flex flex-col  lg:h-24 h-18 p-2 lg:p-0 items-center justify-center  rounded-xl">
+    <div className="flex flex-col  lg:h-24 h-18 p-2 lg:p-0 items-center justify-center  rounded-xl">
       <div>
-        <Image src={icon} width={30} height={30} alt={category}/>
+        <Image src={icon} width={30} height={30} alt={category} />
       </div>
       <div>
         <h4 className="font-bold text-xs text-center ">{category}</h4>
       </div>
     </div>
-   
   );
 };
 
