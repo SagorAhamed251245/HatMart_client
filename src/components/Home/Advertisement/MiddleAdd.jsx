5d0e971c556307062541cc4ee3baed2179a4175b
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-08-11 00:57:32 +0600

    done

--- src/components/Home/Advertisement/MiddleAdd.jsx
+++ src/components/Home/Advertisement/MiddleAdd.jsx
@@ -1,19 +1,37 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const MiddleAdd = () => {
   return (
-    <section className="">
-      <section className="w-[90%] mx-auto h-[300px] bg-red-300 relative">
+    <section className="w-[90%] mx-auto">
+      <section className="  h-[300px] mb-5 bg-red-300 relative rounded-xl overflow-hidden">
+        <Link href={''}>
         <Image
           src="https://i.ibb.co/P1F7nSx/Grocery-Shoping-1.png"
           layout="fill"
           objectFit="cover"
           alt="Middle Add"
-        ></Image>
+        ></Image></Link>
       </section>
-      <section>
-        <div>
+      <section className="flex gap-5 mb-5">
+        <div className=" h-[300px] w-[60%] relative rounded-xl overflow-hidden">
+          <Link  href={''}><Image
+            src="https://i.ibb.co/jrpz7cr/advantages-to.webp"
+            layout="fill"
+            objectFit="cover"
+            alt="Middle Add"
+          ></Image>
+          </Link>
+        </div>
 
+        <div className="bg-green-300 h-[300px] w-[40%] relative rounded-xl overflow-hidden">
+        <Link  href={''}><Image
+            src="https://i.ibb.co/rv8MkLv/addaas.jpg"
+            layout="fill"
+            objectFit="cover"
+            alt="Middle Add"
+          ></Image>
+          </Link>
         </div>
       </section>
     </section>
