1ace7deea0c443271bf32b63c325cb14deca8ce8
mdasik0 <asikthe1st@gmail.com>
2023-08-16 20:54:15 +0600

    all work done cart card

--- src/app/(primaryLayout)/cart/page.js
+++ src/app/(primaryLayout)/cart/page.js
@@ -1,7 +1,27 @@
+"use client";
 import CartCard from "@/components/Cart/CartCard";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const AddToCart = () => {
+  let Price = "32.12";
+  const name = "apple sider vinager pure fresh";
+  const [total, setTotal] = useState();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('cartItem.json')
+    .then(res => res.json())
+    .then(data => setData(data))
+  },[])
+
+  if (!total) {
+    setTotal(Price);
+  }
+  const updateTotal = (total) => {
+    setTotal(total);
+  };
+  // price will change in future
+
   return (
     <div className="mt-40 mb-20 rounded max-w-[1380px] mx-auto bg-slate-100 p-10 ">
       {/* page title here */}
@@ -13,15 +33,38 @@ const AddToCart = () => {
         </p>
       </div>
       {/* This is the item and total payment section */}
-      <section>
-          {/* card here */}
-          <section className="w-fit my-10">
-            <CartCard></CartCard>
-          </section>
-          {/* total payment here */}
-          <section>
+      <section className="my-10 flex md:flex-row justify-between flex-col gap-10">
+        {/* card here */}
+        <section className="w-3/4 flex flex-col items-start">
+          {
+            data.map(cartItem => <CartCard key={cartItem.id} data={data} cartItem={cartItem}></CartCard>)
+          }
+        </section>
+        {/* total payment here */}
+        <section className="bg-green-100 text-center rounded-lg p-3 w-[350px]">
+          <h3 className="text-2xl font-semibold">Total Payment</h3>
+          <table className="table-fixed m-4 text-start flex items-center justify-center">
+            <tbody className="">
 
-          </section>
+              <tr className="border-b ">
+                <td className="pr-6 w-96 font-bold">Product </td>
+                <td className="font-bold">Price</td>
+              </tr>
+              
+              <tr>
+                {/* <td className="pr-6 overflow-hidden">{name.length > 24 ? (name.substring(0,24) + '..') : name}</td>
+                <td>${total}</td> */}
+              </tr>
+
+              <tr className=" border-t-2  border-black">
+                <td className="pr-6 font-semibold">Total Price</td>
+                <td>$300</td>
+              </tr>
+
+
+            </tbody>
+          </table>
+        </section>
       </section>
     </div>
   );
--- src/components/Cart/CartCard.jsx
+++ src/components/Cart/CartCard.jsx
@@ -1,21 +1,44 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { FaRegStar, FaStar } from "react-icons/fa";
 import Rating from "react-rating";
 import { BiTrashAlt } from "react-icons/bi";
 
-const CartCard = () => {
+const CartCard = ({ updateTotal, cartItem }) => {
+  const [quantity, setQuantity] = useState(cartItem?.quantity);
+  const basic = quantity * parseFloat(cartItem.price);
+  const [total, setTotal] = useState(basic);
+  const totalPrice = useRef();
+  const counter = useRef();
+
+  const handlePlusCounter = () => {
+    setQuantity(quantity + 1);
+    const totalPrice = (quantity + 1) * parseFloat(cartItem.price);
+
+    const formattedTotalPrice = totalPrice.toFixed(2);
+    setTotal(formattedTotalPrice);
+  };
+  const handleMinusCounter = () => {
+    if (quantity == 1) {
+      return;
+    }
+    setQuantity(quantity - 1);
+    const totalPrice = (quantity - 1) * parseFloat(cartItem.price);
+
+    const formattedTotalPrice = totalPrice.toFixed(2);
+    setTotal(formattedTotalPrice);
+  };
   return (
-    <div className="bg-white flex p-3 rounded-lg">
+    <div className="bg-white my-3 flex p-3 w-[520px] rounded-lg">
       {/* card image here */}
-      <section className="relative h-[100px] w-[115px] rounded-lg border-2 border-slate-400">
+      <section className="relative h-[100px] w-[100px] rounded-lg border-2 border-slate-400">
         <Image
           fill
           alt="cart image"
           className="object-cover object-center rounded-lg"
-          src={"https://i.ibb.co/37mp1RR/image.png"}
+          src={cartItem?.image}
         ></Image>
       </section>
 
@@ -23,54 +46,65 @@ const CartCard = () => {
       <section className="px-3 flex justify-between">
         {/* item info */}
         <div>
-          <h4 className="text-xl font-semibold">
-            apple sider vinager pure fresh
-          </h4>
+          <h4 className="text-xl font-semibold">{cartItem?.name}</h4>
           <div className="flex items-start mt-2 gap-2">
             <Rating
-              placeholderRating={4.5}
+              placeholderRating={cartItem?.rating}
               emptySymbol={<FaRegStar className="text-yellow-400" />}
               placeholderSymbol={<FaStar className="text-yellow-400" />}
               fullSymbol={<FaStar className="text-yellow-400" />}
               readonly
             />
-            <span className="text-gray-500 text-sm font-medium">({4.5})</span>
+            <span className="text-gray-500 text-sm font-medium">
+              ({cartItem?.rating})
+            </span>
           </div>
           <div>
-            <span className="text-[#34B701] font-bold text-lg">$32.12</span>
+            <span className="text-[#34B701] font-bold text-lg">
+              ${cartItem?.price}
+            </span>
           </div>
         </div>
 
         {/* counter + total */}
 
-        <div className="flex gap-6 ml-10">
+        <div className="flex gap-6 ml-10 ">
           <div className="flex flex-col justify-between items-end">
             {/* counter */}
             <div className="border-2 flex items-center font-semibold border-[#34B701] w-fit rounded">
-              <button className="text-[#34B701] hover:bg-green-500 duration-200  active:bg-green-200  px-[10px] py-1">
+              <button
+                onClick={() => handleMinusCounter()}
+                className="text-[#34B701] hover:bg-green-500 duration-200  active:bg-green-200  px-[10px] py-1"
+              >
                 -
               </button>
-              <span className="px-[10px] border-l-2 border-r-2 ">01</span>
-              <button className="text-[#34B701] hover:bg-green-500 duration-200  active:bg-green-200  px-[10px] py-1">
+              <span ref={counter} className="px-[10px] border-l-2 border-r-2">
+                {quantity}
+              </span>
+              <button
+                onClick={() => handlePlusCounter()}
+                className="text-[#34B701] hover:bg-green-500 duration-200  active:bg-green-200  px-[10px] py-1"
+              >
                 +
               </button>
             </div>
 
             {/* total */}
             <div>
-
               <div className="text-center inline-block text-slate-500 mr-10">
-                <span className="block text-sm">Total</span>
-                <span className=" font-semibold">$30</span>
-              </div>
-
-              <div className="text-center inline-block text-slate-500">
                 <span className="block text-sm">Unit</span>
                 <span className=" font-semibold">kg</span>
               </div>
-
+              <div className="text-center inline-block text-slate-500 ">
+                <span className="block text-sm">Total</span>
+                <span>
+                  $
+                  <span ref={totalPrice} className="total font-semibold">
+                    {total}
+                  </span>
+                </span>
+              </div>
             </div>
-
           </div>
 
           <button className="bg-red-500 hover:bg-red-400 active:bg-red-600 text-white p-3 text-2xl h-fit rounded-full my-auto">
