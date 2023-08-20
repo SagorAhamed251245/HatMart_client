e5ede86c439799a3a5558847d7854dd572b9b03d
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-08-20 22:56:44 +0600

    cetegory product some improved

--- src/components/Home/Categories/CategoriesCart.jsx
+++ src/components/Home/Categories/CategoriesCart.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from "react";
 import Image from "next/image";
 import SubCategories from "./SubCategories";
@@ -17,22 +18,22 @@ const CategoriesCart = ({ item }) => {
   return (
     <div className="h-24 w-36  ">
       <div
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      className=" flex flex-col cursor-pointer h-24 w-36  p-2 lg:p-0 items-center justify-center rounded-xl"
-    >
-      <div>
-        <Image src={icon} width={30} height={30} alt={category} />
-      </div>
-      <div>
-        <h4 className="font-bold text-xs text-center">{category}</h4>
-      </div>
-      {showSubCategory && (
-        <div className=" h-auto  absolute z-30  left-30 top-52 ">
-          <SubCategories sub_category={sub_category}></SubCategories>
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        className=" flex flex-col cursor-pointer h-24 w-36  p-2 lg:p-0 items-center justify-center rounded-xl"
+      >
+        <div>
+          <Image src={icon} width={30} height={30} alt={category} />
         </div>
-      )}
-    </div>
+        <div>
+          <h4 className="font-bold text-xs text-center">{category}</h4>
+        </div>
+        {showSubCategory && (
+          <div className=" h-auto  absolute z-30   top-40 ">
+            <SubCategories sub_category={sub_category}></SubCategories>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
