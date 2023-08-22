883ee94d371005acd7e46e01a2031f7abb32f237
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-08-22 11:32:59 +0600

    product details polish

--- src/components/Home/Products/ProductDetailsPage/ProductDetails.jsx
+++ src/components/Home/Products/ProductDetailsPage/ProductDetails.jsx
@@ -1,14 +1,13 @@
-'use client'
+"use client";
 import Link from "next/link";
 import React from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { FaRegStar, FaStar } from "react-icons/fa";
 import Rating from "react-rating";
 
-
 const ProductDetails = ({ productData }) => {
   return (
-    <div className=" w-full">
+    <div className=" w-full h-full">
       <h3 className="text-gray-700 text-4xl font-medium">
         {productData?.title}
       </h3>
@@ -26,8 +25,26 @@ const ProductDetails = ({ productData }) => {
           <span className="text-[#32B900] "> (1 customer review)</span>
         </div>
       </div>
-      <p className="text-[#34B701] font-medium text-2xl mt-4">
-        ${productData?.price}
+      <p className="font-medium text-2xl mt-4">
+        {productData?.discount_percent ? (
+          <>
+            <span className=" line-through !text-gray-500">
+              ${productData?.price}
+            </span>
+            <span className="text-[#34B701] ml-4">
+              $
+              {(
+                parseFloat(productData?.price) -
+                parseFloat(
+                  parseFloat(productData?.price) *
+                    (parseFloat(productData?.discount_percent) / 100)
+                )
+              ).toFixed(2)}
+            </span>{" "}
+          </>
+        ) : (
+          <span className="text-[#34B701]">${productData?.price}</span>
+        )}
       </p>
       <p className="text-base my-3 text-gray-500 dark:text-white">
         {productData?.details?.description}
@@ -40,19 +57,6 @@ const ProductDetails = ({ productData }) => {
         {productData?.stock_quantity} in stock
       </p>
 
-      <div className="flex items-center gap-5 !mt-5">
-        <button className="flex justify-center items-center gap-2 text-[#34B701] font-medium text-2xl bg-green-100 px-6 py-1.5 rounded hover:bg-green-200">
-          <AiOutlineShoppingCart size={24} /> Add{" "}
-        </button>
-        <Link href={"/payment"}>
-          <button
-            disabled={productData?.stock === "Out of stock"}
-            className="flex justify-center items-center gap-2 bg-[#ff6347cc]  text-white px-6 py-1.5 rounded text-2xl  disabled:opacity-60 hover:bg-[#FF7B13]"
-          >
-            Buy Now
-          </button>
-        </Link>
-      </div>
       <p className="text-base mb-3 mt-5 text-gray-500 dark:text-white">
         Category:{" "}
         <span className="text-green-500">{productData?.category}</span>
@@ -68,6 +72,19 @@ const ProductDetails = ({ productData }) => {
             ))}
         </span>
       </p>
+      <div className="flex items-center gap-5 !mt-5">
+        <button className="flex justify-center items-center gap-2 text-[#34B701] font-medium  bg-green-100 px-6 py-1.5 rounded hover:bg-green-200">
+          <AiOutlineShoppingCart size={24} /> Add{" "}
+        </button>
+        <Link href={"/payment"}>
+          <button
+            disabled={productData?.stock === "Out of stock"}
+            className="flex justify-center items-center gap-2 bg-[#ff6347cc]  text-white px-6 py-1.5 rounded   disabled:opacity-60 hover:bg-[#FF7B13]"
+          >
+            Buy Now
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
