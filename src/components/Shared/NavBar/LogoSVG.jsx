1d078deba16c4f9eb99fe0eafc2db165154c57ba
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-08-23 18:03:25 +0600

    logo link added

--- src/components/Shared/NavBar/LogoSVG.jsx
+++ src/components/Shared/NavBar/LogoSVG.jsx
@@ -1,15 +1,18 @@
 import logo from "@/assets/icons/hatmartLogo.svg";
 import Image from "next/image";
+import Link from "next/link";
 
 const LogoSVG = () => {
   return (
     <div className="relative h-10 md:h-14 w-10 md:w-14">
-      <Image
-        src={logo}
-        layout="fill"
-        objectFit="cover"
-        alt="HatMart Logo"
-      ></Image>
+      <Link href={"/"}>
+        <Image
+          src={logo}
+          layout="fill"
+          objectFit="cover"
+          alt="HatMart Logo"
+        ></Image>
+      </Link>
     </div>
   );
 };
