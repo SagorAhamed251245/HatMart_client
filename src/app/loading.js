b1e9b633295abb27a81c2ab595e8bbbe5a7eaf00
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-09-05 22:20:42 +0600

    logind error

--- src/app/loading.js
+++ src/app/loading.js
@@ -5,7 +5,7 @@ const Loading = () => {
   return (
     <>
       <div className=" flex items-center h-screen justify-center animate-bounce">
-        <Image src={logo} width={100} height={100} alt="logo" srcset="" />
+        <Image src={logo} width={100} height={100} alt="logo" srcSet="" />
         <div className="text-[#FF6E14] font-bold text-xl">loading...</div>
       </div>
     </>
