b83412e062b4f2dbb7e69dff5d59559683e198b9
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-09-08 12:03:22 +0600

    added test case and admin dashboard page

---
+++ src/components/Dashboard/Admin/AdminDashboard/OverView/OverView.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SalesStatistics from "../SalesStatistics";
+import OverViewChart from "./OverViewChart";
+
+const OverView = () => {
+  return (
+    <div>
+      <SalesStatistics />
+      <OverViewChart />
+    </div>
+  );
+};
+
+export default OverView;
