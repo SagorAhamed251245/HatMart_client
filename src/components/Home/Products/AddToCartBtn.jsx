eaf152325cab3e4962a0d49a1f4b7baee2a86e17
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-09-09 12:42:48 +0600

    some update done

--- src/components/Home/Products/AddToCartBtn.jsx
+++ src/components/Home/Products/AddToCartBtn.jsx
@@ -4,12 +4,14 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 const AddToCartBtn = ({ handleAddToCart, id }) => {
   return (
     <>
-      <button
-        onClick={() => handleAddToCart(id)}
-        className="flex justify-center items-center gap-2 text-[#34B701] font-medium bg-green-100 text-sm md:text-base px-[10px] md:px-4 py-1 rounded hover:bg-green-200"
-      >
-        <AiOutlineShoppingCart size={20} /> Add
-      </button>
+      <Link>
+        <button
+          onClick={() => handleAddToCart(id)}
+          className="flex justify-center items-center gap-2 text-[#34B701] font-medium bg-green-100 text-sm md:text-base px-[10px] md:px-4 py-1 rounded hover:bg-green-200"
+        >
+          <AiOutlineShoppingCart size={20} /> Add
+        </button>
+      </Link>
     </>
   );
 };
