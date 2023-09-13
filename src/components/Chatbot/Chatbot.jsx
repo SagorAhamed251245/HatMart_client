fbce66bca56e5316228ca96e25c32bd03ba0f181
anighosal <121676026+anighosal@users.noreply.github.com>
2023-09-13 22:50:38 +0600

    chatbot complete

--- src/components/Chatbot/Chatbot.jsx
+++ src/components/Chatbot/Chatbot.jsx
@@ -4,7 +4,6 @@ import { FacebookProvider, CustomChat } from "react-facebook";
 
 const Chatbot = () => (
   <div>
-    {/* <MessengerCustomerChat pageId="138874569299940" appId="289867620446215" /> */}
     <FacebookProvider appId="624884016421394" chatSupport>
       <CustomChat pageId="138874569299940" minimized={true} />
     </FacebookProvider>
