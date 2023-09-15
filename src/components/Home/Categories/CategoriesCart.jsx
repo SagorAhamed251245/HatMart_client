a761280f87720b672f0969d024abf559c8a42c07
fahad <99122881+fahad4161@users.noreply.github.com>
2023-09-15 16:52:09 +0600

    navbar added

--- src/components/Home/Categories/CategoriesCart.jsx
+++ src/components/Home/Categories/CategoriesCart.jsx
@@ -3,22 +3,14 @@ import React, { useState } from "react";
 import Image from "next/image";
 import SubCategories from "./SubCategories";
 
-const CategoriesCart = ({ item }) => {
+const CategoriesCart = ({ item, handleMouseEnter, handleMouseLeave }) => {
   const { id, category, icon, sub_category } = item;
-  const [showSubCategory, setShowSubCategory] = useState(false);
-
-  const handleMouseEnter = () => {
-    setShowSubCategory(true);
-  };
-
-  const handleMouseLeave = () => {
-    setShowSubCategory(false);
-  };
+ 
 
   return (
     <div className="h-24 w-full">
       <div
-        onMouseEnter={handleMouseEnter}
+        onMouseEnter={() => handleMouseEnter(sub_category)}
         onMouseLeave={handleMouseLeave}
         className=" flex flex-col cursor-pointer h-24 w-36  p-2 lg:p-0 items-center justify-center rounded-xl"
       >
@@ -27,12 +19,7 @@ const CategoriesCart = ({ item }) => {
         </div>
         <div>
           <h4 className="font-bold text-xs text-center">{category}</h4>
-        </div>
-        {showSubCategory && (
-          <div className=" h-auto  absolute z-30 w-auto bg-white shadow-2xl left-10 right-10 top-40 ">
-            <SubCategories sub_category={sub_category}></SubCategories>
-          </div>
-        )}
+        </div>        
       </div>
     </div>
   );
