62f00cfb1b440370e1bc931df8c98ef15ac9a031
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-09-16 23:18:45 +0600

    firebase env

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage"; // Import getStorage instead of s
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAA0t-adIUYRMztD6oV0R3GikVDv1xybmw",
-  authDomain: "hatmart-e8316.firebaseapp.com",
-  projectId: "hatmart-e8316",
-  storageBucket: "hatmart-e8316.appspot.com",
-  messagingSenderId: "897550218427",
-  appId: "1:897550218427:web:a8b67ece723c7b055ca59e",
-  databaseURL: "gs://hatmart-e8316.appspot.com",
+  apiKey: process.env.NEXT_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_FIREBASE_APP_ID,
+  databaseURL: process.env.NEXT_FIREBASE_DATABASEURL,
 };
 
 // Initialize Firebase
