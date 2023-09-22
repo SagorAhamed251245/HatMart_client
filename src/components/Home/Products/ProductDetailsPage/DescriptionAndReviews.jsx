1329352d853ff4179a79e335f09b9fa46a5d088f
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2023-09-22 19:00:57 +0600

    Update DescriptionAndReviews.jsx

--- src/components/Home/Products/ProductDetailsPage/DescriptionAndReviews.jsx
+++ src/components/Home/Products/ProductDetailsPage/DescriptionAndReviews.jsx
@@ -6,7 +6,7 @@ const DescriptionAndReviews = ({ productData, reviewsData }) => {
   return (
     <div className="border border-gray-300 rounded-xl p-5 mt-10">
       <Tabs>
-        <TabList className="text-center  mx-auto  flex gap-6 tabs text-gray-600 py-3 duration-500">
+        <TabList className="text-center  mx-auto  flex gap-6 tabs text-gray-600 dark:text-white py-3 duration-500">
           <Tab
             className="font-semibold cursor-pointer  border border-gray-300 rounded-full px-5 py-1 outline-none"
             selectedClassName="text-[#34B701]  border-[#34B701] duration-300"
@@ -15,7 +15,7 @@ const DescriptionAndReviews = ({ productData, reviewsData }) => {
           </Tab>
           <Tab
             className="font-semibold cursor-pointer  border border-gray-300 rounded-full px-5 py-1 outline-none"
-            selectedClassName="text-[#34B701]  border-[#34B701] duration-300"
+            selectedClassName="text-[#34B701] dark:text-white border-[#34B701] duration-300"
           >
             Reviews ({reviewsData?.length || 0})
           </Tab>
@@ -23,19 +23,19 @@ const DescriptionAndReviews = ({ productData, reviewsData }) => {
 
         <TabPanel>
           <div className="mt-8">
-            <p className="text-gray-500">{productData?.details?.description}</p>
+            <p className="text-gray-500 dark:text-white">{productData?.details?.description}</p>
 
             <div className="mt-10">
-              <h3 className="text-gray-700 font-medium text-xl">
+              <h3 className="text-gray-700 font-medium text-xl dark:text-white">
                 Packaging & Delivery
               </h3>
-              <p className="text-gray-500 mt-4">
+              <p className="text-gray-500 mt-4 dark:text-white">
                 {productData?.details?.packagingDelivery}
               </p>
             </div>
             <div className="mt-10">
-              <h3 className="text-gray-700 font-medium text-xl">Warnings</h3>
-              <p className="text-gray-500 mt-4">
+              <h3 className="text-gray-700 font-medium text-xl dark:text-white">Warnings</h3>
+              <p className="text-gray-500 mt-4 dark:text-white">
                 {productData?.details?.warnings}
               </p>
             </div>
