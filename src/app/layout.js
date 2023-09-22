7bd3ec518165226049d05e53681ed20f26c21c95
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-09-22 18:46:49 +0600

    sss

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ const RootLayout = ({ children }) => {
     <html lang="en" data-theme="dark" className="transition-all">
       <Providers>
         <body
-          className={`${poppins.className} scrollbar  `}
+          className={`${poppins.className} scrollbar  select-none dark:text-white`}
         >
           {children}
           <Toaster />
